72807aa047c74a2aa1d70a5212debdf54e9db09d
codelikeagirl29 <codelikeagirl91@gmail.com>
2024-02-27 06:52:28 -0600

    add readme

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import Accordion from "./Accordion";
+
+export default function Card() {
+	return (
+		<div className="container">
+			<div className="card">
+				<Accordion></Accordion>
+			</div>
+		</div>
+	);
+}
